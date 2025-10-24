93021350dac102ff1722614d9da99c93123325fe
pedasmith <pedasmith@yahoo.com>
2025-10-23 18:05:52 -0700

    Auto-size complete; will create a Type I graph that scales according to the height of the SVG element

--- ExampleHtml/Nomograph.js
+++ ExampleHtml/Nomograph.js
@@ -140,25 +140,92 @@
                 this.parent = this.ElementCreate(this.svg);
                 this.DrawLinePixel(this.parent, this.x_pixel, this.ystart_pixel, this.x_pixel, this.ystart_pixel+this.height_pixel, this.linestyle);
                 this.DrawTitlePixel(this.parent, this.title, this.x_pixel, this.ystart_pixel);
-
                 this.ticks.DrawTicks(this);
             }            
         }
-        function MakePage_30Diagram(svg)
+
+        class NomographTypeI
         {
-            let Width = 80;
-            let HPerUnit = 50;
+            constructor(svg, umin, umax, vmin, vmax)
+            {
+                this.svg = svg;
+                this.umin = umin;
+                this.umax = umax;
+                this.vmin = vmin;
+                this.vmax = vmax;
+                this.wmin = this.umin + this.vmin;
+                this.wmax = this.umax + this.vmax;
 
-            let U = new Axis (svg, "U", 20, 50, HPerUnit*4, 0.0, 4.0, "U", "left"); 
-            U.DrawGraduations();
+                this.HTitle_pixel = 20;
+                this.HFooter_pixel = 20;
 
-            let V = new Axis (svg, "V", 20+2*Width, 50, HPerUnit*6, 0.0, 6.0, "V", "right"); 
-            V.DrawGraduations();
+                this.AxisW_pixel = 80;
 
-            let W = new Axis (svg, "W", 20+1*Width, 50, HPerUnit*5, 0.0, 10.0, "W", "right"); 
-            W.DrawGraduations();
+            }
+
+            Initialize()
+            {
+                //let bbox = this.svg.getBBox(); // Nope; this is the height after all the objects are placed
+                let height= this.svg.getAttribute("height");
+                console.log(`DBG: Initialize: height=${height} `);
+
+                this.XU_pixel = 20;
+                this.XW_pixel = this.XU_pixel + this.AxisW_pixel;
+                this.XV_pixel = this.XU_pixel + 2*this.AxisW_pixel;
+
+                let h_pixel = height - this.HTitle_pixel - this.HFooter_pixel;
+                let h_per_unit_u = h_pixel / (this.umax - this.umin); 
+                let h_per_unit_v = h_pixel / (this.vmax - this.vmin);
+                let h_per_unit = h_per_unit_u < h_per_unit_v ? h_per_unit_u : h_per_unit_v; 
+
+                this.U = new Axis (this.svg, "U", 
+                    this.XU_pixel, 
+                    this.HTitle_pixel, h_per_unit*(this.umax-this.umin), 
+                    this.umin, this.umax, 
+                    "U", "left"); 
+                this.U.DrawGraduations();
+
+                this.V = new Axis (this.svg, "V", 
+                    this.XV_pixel, 
+                    this.HTitle_pixel, h_per_unit*(this.vmax-this.vmin), 
+                    this.vmin, this.vmax, 
+                    "V", "right"); 
+                this.V.DrawGraduations();
+
+                this.W = new Axis (this.svg, "W", 
+                    this.XW_pixel, 
+                    this.HTitle_pixel, h_per_unit*(this.wmax-this.wmin) / 2, 
+                    this.wmin, this.wmax, 
+                    "W", "right"); 
+               this.W.DrawGraduations();
+            }
+
+
+            ZZZMakePage_30Diagram(svg)
+            {
+                var nomograph = new NomographTypeI(svg);
+                let Width = 80;
+                let HPerUnit = 50;
+
+                let U = new Axis (svg, "U", 20, 50, HPerUnit*4, 0.0, 4.0, "U", "left"); 
+                U.DrawGraduations();
+
+                let V = new Axis (svg, "V", 20+2*Width, 50, HPerUnit*6, 0.0, 6.0, "V", "right"); 
+                V.DrawGraduations();
+
+                let W = new Axis (svg, "W", 20+1*Width, 50, HPerUnit*5, 0.0, 10.0, "W", "right"); 
+                W.DrawGraduations();
+            }
 
         }
+
+        function MakePage_30Diagram(svg)
+        {
+            var nomograph = new NomographTypeI(svg, 0.0, 4.0, 0.0, 6.0);
+            nomograph.Initialize();
+            return nomograph;
+        }
+
         function OnLoad()
         {
             let svg = document.getElementById("nomo");
---
+++ ExampleHtml/TODO.md
@@ -0,0 +1,14 @@
+# TODO file for ExampleHtml for Nomography
+
+Contains exploratory code for making a variety of Nomographs. Examples are from "Elements of Nomography", first edition, 1947 by Raymond D. Douglass and Douglass P. Adams, both professors at MIT, published by McGraw-Hill Book company
+
+## Simpke TODO items
+
+Resize. On size changes, redraw.
+DONE: 2025-10-23: Auto-size. Graph should have space for title + footer, and the rest of the SVG should be for the diagram
+
+## Can the exported SVG be used for laser engraving?\
+
+Maybe? Can I import the SVG into Inkscape and then exort EPS?
+
+See [Edge](https://blogs.windows.com/msedgedev/2024/07/11/seamless-svg-copy-paste-on-the-web/) for help?
